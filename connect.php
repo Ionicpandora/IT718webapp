38ee349c13fd7913703212650591079213c7b458
Austin <an1201@usnh.edu>
2024-04-15 19:01:08 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 try {
     $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "attendancetracker1-server.mysql.database.azure.com", "qivtdipzbu", "P$Oljub5Z2wAY4o8", "group", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "attendencetracker1.mysql.database.azure.com", "qivtdipzbu", "IT09123@dmin", "group", 3306, MYSQLI_CLIENT_SSL);
 
 } catch (\Throwable $th) {
     include 'error.html';
