f502425e598d1c45020d038a175b350f41fee96d
Ionicpandora <123591220+Ionicpandora@users.noreply.github.com>
2024-04-15 13:56:40 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,3 +2,4 @@
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($conn, "attendancetracker1-server.mysql.database.azure.com", "qivtdipzbu", "P$Oljub5Z2wAY4o8", "group", 3306, MYSQLI_CLIENT_SSL);
+?>
