ffd6ce273009174870e9d1e80752780ca94ed619
Austin <an1201@usnh.edu>
2024-04-15 16:16:25 -0400

    debugging php connections

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ if(isset($_REQUEST['group'])){
   $group_to_add = htmlspecialchars($_REQUEST['group']);
   $name = htmlspecialchars($_REQUEST['person']);
   include "addperson.php";
-  exit();
 
 }elseif(isset($_REQUEST['create'])){
   echo "works";
