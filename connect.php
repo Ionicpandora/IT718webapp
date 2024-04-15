c49e306f2678fc1fd5c02e34d3aba62108f295fd
Austin <an1201@usnh.edu>
2024-04-15 16:40:10 -0400

    removed error code

--- connect.php
+++ connect.php
@@ -5,6 +5,5 @@ mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($conn, "attendancetracker1-server.mysql.database.azure.com", "qivtdipzbu", "P$Oljub5Z2wAY4o8", "group", 3306, MYSQLI_CLIENT_SSL);
 
 } catch (\Throwable $th) {
-    echo $th;
     include 'error.html';
 }
