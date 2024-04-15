876e91e614c31e187e79490b4bcd433f9835a266
Ionicpandora <123591220+Ionicpandora@users.noreply.github.com>
2024-04-15 13:40:09 -0400

    Update creategroup.php

--- creategroup.php
+++ creategroup.php
@@ -1,3 +1,17 @@
 <?php
 
 include "connect.php";
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
