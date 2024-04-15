9f0e20502cb50f298d6b5448201518d8f3f0ecb4
Austin <an1201@usnh.edu>
2024-04-15 16:02:22 -0400

    added file and modified file names

---
+++ getattendence.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<a href="homepage.html">Homepage</a>
+  <h1>attendence records</h1>
+  <?php echo "<p>this is php</p>"?>
+
+
+</body>
+</html>
---
+++ takeaddtendance.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>takingattendence</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>Takking attendence of </h1>
+
+</body>
+</html>
