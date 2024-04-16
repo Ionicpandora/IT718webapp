a72cdf6299a88422cc76b9123c9c9cdaa0476110
Austin <an1201@usnh.edu>
2024-04-15 21:44:05 -0400

    Update addgroup.php

--- addgroup.php
+++ addgroup.php
@@ -3,19 +3,19 @@ try {
     $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
+if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 // Run the create table query
-if (mysqli_query($conn, '
-CREATE TABLE Products (
+if (mysqli_query($conn, "
+CREATE TABLE '%{$group_name}%' (
 `Id` INT NOT NULL AUTO_INCREMENT ,
 `person` VARCHAR(225),
 `Price` DOUBLE NOT NULL ,
 PRIMARY KEY (`Id`)
 );
-')) {
+")) {
 printf("Table created\n");
 }
 
