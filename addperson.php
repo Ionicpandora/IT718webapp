134fbc78bc9a7cf714ab516248ea023c12f42956
Austin <an1201@usnh.edu>
2024-04-16 17:43:28 -0400

    Update addperson.php

--- addperson.php
+++ addperson.php
@@ -10,8 +10,9 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 //Create an Insert prepared statement and run it
 $group_name = "test";
 $person = 'austin';
-if ($stmt = mysqli_prepare($conn, "INSERT INTO ? (person) VALUES (?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $group_name, $person);
+$price = 12.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO ? (person) VALUES (?, ?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $group_name, $person, $price);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
 mysqli_stmt_close($stmt);
