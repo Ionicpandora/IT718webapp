81470a41ce6bb9be32b78cd575ea38da1d4a61a0
Austin <an1201@usnh.edu>
2024-04-15 21:20:41 -0400

    made forms required and tried new connect method

--- addgroup.php
+++ addgroup.php
@@ -1,5 +1,12 @@
 <?php
 try {
+    $conn = mysqli_init();
+    mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
+    if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+
 $host = 'attendencetracker1.mysql.database.azure.com';
 $username = 'qivtdipzbu';
 $password = 'IT09123@dmin';
