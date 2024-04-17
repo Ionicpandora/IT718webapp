740d2bc34ff0fb88c9abc655fd109d7b9c0b33dd
Austin <an1201@usnh.edu>
2024-04-17 15:50:44 -0400

    Update getattendencerecords.php

--- getattendencerecords.php
+++ getattendencerecords.php
@@ -10,7 +10,7 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 
 // Run the create table query
 if ($results = mysqli_query($conn, "
-SELECT * FROM {$viewgroup} WHERE group={$viewgroup};
+SELECT * FROM savedAttendence WHERE group={$viewgroup};
 ")) {
 printf("attendence saved\n");
 }
