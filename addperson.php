7665534f7f6743126642c5a23772c72a8856b08b
Austin <an1201@usnh.edu>
2024-04-17 12:28:41 -0400

    Update addperson.php

--- addperson.php
+++ addperson.php
@@ -7,11 +7,21 @@ if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-if (mysqli_query($conn, "
+/*if (mysqli_query($conn, "
 INSERT INTO {$group_name} (person) VALUES ('{$name}');
 ")) {
 printf("Person added to table\n");
+}*/
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+mysqli_stmt_execute($stmt);
+printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+mysqli_stmt_close($stmt);
 }
+
 //Close the connection
 mysqli_close($conn);
 } catch (\Throwable $th) {
