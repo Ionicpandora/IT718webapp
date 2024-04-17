6a46f99b7ee454cd430c9a9043f368cc1525bf30
Austin <an1201@usnh.edu>
2024-04-17 12:38:53 -0400

    Update addperson.php

--- addperson.php
+++ addperson.php
@@ -7,13 +7,10 @@ if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-/*if (mysqli_query($conn, "
-INSERT INTO {$group_name} (person) VALUES ('{$name}');
-")) {
-printf("Person added to table\n");
-}*/
-if ($stmt = mysqli_prepare($conn, "INSERT INTO {$group_to_add} (person) VALUES (?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $name);
+$group = $group_to_add;
+$person = $name;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO {$group} (person) VALUES (?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $person);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
 mysqli_stmt_close($stmt);
