58fc47bc0dbb5ab27e46f477fce4c13f43385cfe
Austin <an1201@usnh.edu>
2024-04-18 14:52:41 -0400

    Update viewattendence.html.php

--- viewattendence.html.php
+++ viewattendence.html.php
@@ -7,14 +7,15 @@
 <body>
 
 <h1>Viewing attendence records of <?php echo $viewgroup ?></h1>
+<a href="homepage.html">Homepage</a>
 <table>
 <?php print_r($results); foreach($results as $result):?>
     <tr>
-    <td><?php echo $result["id"]?></td>
-    <td><?php echo $result["groupattended"]?></td>
-    <td><?php echo $result["person"]?></td>
-    <td><?php echo $result["dateattended"]?></td>
-    <td><?php echo $result["attendend"]?></td>
+        <td><?php echo $result["id"]?></td>
+        <td><?php echo $result["groupattended"]?></td>
+        <td><?php echo $result["person"]?></td>
+        <td><?php echo $result["dateattended"]?></td>
+        <td><?php echo $result["attendend"]?></td>
     </tr>
     <?php endforeach; ?>
 </table>
