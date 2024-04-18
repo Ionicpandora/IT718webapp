2a6efe68c1e1e01bcdff17a9bac070e41f1f58bb
Austin <an1201@usnh.edu>
2024-04-18 14:06:36 -0400

    Update addgroup.php

--- addgroup.php
+++ addgroup.php
@@ -9,7 +9,7 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 // Run the create table query
-/*if (mysqli_query($conn, "
+if (mysqli_query($conn, "
 CREATE TABLE {$group_name} (
 `Id` INT NOT NULL AUTO_INCREMENT ,
 `person` VARCHAR(225),
@@ -17,16 +17,16 @@ PRIMARY KEY (`Id`)
 );
 ")){
 printf("Table created\n");
-}*/
+}
 
-if(mysqli_query($conn, "CREATE Table savedAttendence (
+/*if(mysqli_query($conn, "CREATE Table savedAttendence (
     `Id` INT NOT NULL AUTO_INCREMENT ,
     `groupattended` VARCHAR(255),
     `person` VARCHAR(225),
     `dateattended` DATE,
     `attendend` BIT,
     PRIMARY KEY (`Id`)
-)"))
+)"))*/
 
 //Close the connection
 mysqli_close($conn);
