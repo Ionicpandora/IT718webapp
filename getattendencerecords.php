f879407f211d4f9e251becfcb684dda8662aa1f4
Austin <an1201@usnh.edu>
2024-04-19 19:18:59 -0400

    combine connection queries to one file

--- getattendencerecords.php
+++ getattendencerecords.php
@@ -1,13 +1,8 @@
 <?php
 
 try {
-    $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
+    include 'connect.php';
+    
 // Run the create table query
 if ($results = mysqli_query($conn, "
 SELECT * FROM savedAttendence;
