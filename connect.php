17d5af1bef22a60809755097444f73ad7a79d69a
Austin <an1201@usnh.edu>
2024-04-22 16:37:57 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,25 +1,24 @@
 <?php 
 
 $secret ='<script type="text/JavaScript">  
-const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
-
-const credential = new DefaultAzureCredential();
-
-const vaultName = "AttendenceTracker1-Vault";
-const url = `https://${vaultName}.vault.azure.net`;
-
-const client = new SecretClient(url, credential);
-
-const secretName = "Admin-Secrect";
+const { DefaultAzureCredential } = require("@azure/identity");
 
 async function main() {
-  const latestSecret = await client.getSecret(secretName);
-  return latestSecret;
-}
+  const credential = new DefaultAzureCredential();
 
-await main();
-     </script>' 
+  const keyVaultName = "AttendenceTracker1-Vault";
+  const url = "https://" + keyVaultName + ".vault.azure.net";
+
+  const client = new SecretClient(url, credential);
+
+  const secretName = "Admin-Secrect"
+
+  const secret = await client.getSecret(secretName);
+  console.log("secret: ", secret);
+return secret;
+}
+ </script>' 
 ; 
 
 $value = $secret["value"];
