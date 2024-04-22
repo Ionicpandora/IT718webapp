32cecdcb43ddd0a0ed8a79464a7ae4362fbeaa8a
Austin <an1201@usnh.edu>
2024-04-22 12:27:53 -0400

    removed failed key vault usage attempt

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 use Azure\Identity\DefaultAzureCredential;
 use Azure\Security\KeyVault\Secrets\SecretClient;
 
-$keyVaultUrl = "https://attendencetracker1-vault.vault.azure.net/";
+$keyVaultUrl = "https://attendencetracker1-vault.vault.azure.net/secrets/Admin-Secrect/2bce63b9e0a2430990aeb79b506ef675";
 $secretName = "Admin-Secret";
 
 $credential = new DefaultAzureCredential();
@@ -17,7 +17,7 @@ echo $connString;
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', `{$connString}`, 'group', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
