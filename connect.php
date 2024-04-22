42934e620324f49301884750fd42768c683cb9ee
Austin <an1201@usnh.edu>
2024-04-22 13:04:21 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,5 @@
 <?php 
 
-$secret = "https://attendencetracker1-vault.vault.azure.net/secrets/Admin-Secrect/2bce63b9e0a2430990aeb79b506ef675";
-
-echo $secret;
-
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
