eadf8ab2f5586f65ac3b72399df5b0c0594bd21b
Austin <an1201@usnh.edu>
2024-04-22 13:51:25 -0400

    removed bad code

--- connect.php
+++ connect.php
@@ -1,8 +1,5 @@
 <?php 
-$secret = new AzKeyVault\Secret('https://attendencetracker1-vault.vault.azure.net');
-$value = $secret->getSecret('Admin-Secrect');
 
-echo $value;
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
