f3ec82a38d624f4d96664312918bc3014556bf5c
Austin <an1201@usnh.edu>
2024-04-22 16:15:53 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -15,9 +15,7 @@ const secretName = "Admin-Secrect";
 
 async function main() {
   const latestSecret = await client.getSecret(secretName);
-  console.log(`Latest version of the secret ${secretName}: `, latestSecret);
-  const specificSecret = await client.getSecret(secretName, { version: latestSecret.properties.version! });
-  console.log(`The secret ${secretName} at the version ${latestSecret.properties.version!}: `, specificSecret);
+  return latestSecret;
 }
 
 await main();
