a4ce1967283d1e9587b8218ecb8adf197775efbf
Austin <an1201@usnh.edu>
2024-04-22 15:40:07 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,31 @@
 <?php 
 
+$secret ='<script type="text/JavaScript">  
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");x
+
+const credential = new DefaultAzureCredential();
+
+const vaultName = "AttendenceTracker1-Vault";
+const url = `https://${vaultName}.vault.azure.net`;
+
+const client = new SecretClient(url, credential);
+
+const secretName = "Admin-Secrect";
+
+export default async function main() {
+  const latestSecret = await client.getSecret(secretName);
+  console.log(`Latest version of the secret ${secretName}: `, latestSecret);
+  const specificSecret = await client.getSecret(secretName, { version: latestSecret.properties.version! });
+  console.log(`The secret ${secretName} at the version ${latestSecret.properties.version!}: `, specificSecret);
+}
+
+main();
+     </script>' 
+; 
+
+echo $secret;
+$secret =
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', 'IT09123@dmin', 'group', 3306, MYSQLI_CLIENT_SSL);
