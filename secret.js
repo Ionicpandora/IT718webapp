80dc41179366e8376f31b69f45c713123834de44
Austin <an1201@usnh.edu>
2024-04-28 21:32:41 -0400

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,5 +1,8 @@
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
+const { KeyClient } = require('@azure/keyvault');
+
+const keyClient = new KeyClient('https://AttendenceTracker1-Vault.vault.azure.net');
 
 const credential = new DefaultAzureCredential();
 
@@ -10,9 +13,17 @@ const client = new SecretClient(url, credential);
 
 const secretName = "Admin-Secrect";
 
-async function main() {
-  const latestSecret = await client.getSecret(secretName);
-  return latestSecret.value;
+function main() {
+    const secretName = 'Admin-Secrect';
+    const version = '2bce63b9e0a2430990aeb79b506ef675';
+
+  keyClient.getKey(secretName, version).then((result) => {
+    const secretValue = result.value;
+    // Use the secret value here
+    return secretValue;
+  }).catch((error) => {
+    console.error(error);
+  });
 }
 
-await main();
+main();
