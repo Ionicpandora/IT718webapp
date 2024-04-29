cc0bc3c8bde9a0b67465253fcc836eb8e1ba5c5f
Austin <an1201@usnh.edu>
2024-04-28 21:03:15 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php 
 
 $secret = '<script src="secret.js"></script>';
+$value = $secret.data()
 
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', '{$secret}', 'group', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', '{$value}', 'group', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
