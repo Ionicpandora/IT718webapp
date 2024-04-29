480bdeaf51f761ce31f7ba51aa696cdc11723c7b
Austin <an1201@usnh.edu>
2024-04-29 10:05:31 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 $secret = '<script src="secret.js"></script>';
 
-$value = serialize($secret);
+$value = $secret->getValue();
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'attendencetracker1.mysql.database.azure.com', 'qivtdipzbu', '{$value}', 'group', 3306, MYSQLI_CLIENT_SSL);
